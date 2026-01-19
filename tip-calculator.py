7775c4713805dfd87ceee6b5c12143e4ae985e24
YUVARAJ M S <yuvarajgowda.official@gmail.com>
2026-01-20 00:16:11 +0530

    Initial Commit: Tip Calculator

--- tip-calculator.py
+++ tip-calculator.py
@@ -7,10 +7,10 @@ while True:
         people=int(input("How many people to split the bill:"))
         bill_after_tip=(bill_before_tip*tip/100)+bill_before_tip
         print(f"Total bill: ${bill_after_tip:.2f}")
-        bill_perhead=bill_after_tip/people
-        print(f"Bill per person: ${bill_perhead:.2f}")
+        bill_spearhead= bill_after_tip / people
+        print(f"Bill per person: ${bill_spearhead:.2f}")
         break
     except ZeroDivisionError:
-            print("Total bill cannot be splitted among [0] people")
+            print("Total bill cannot be split among [0] people")
     except ValueError:
         print("You typed an invalid input")
